a1d6199db38966e5458d524f8f6299ec50c81614
belencepeda12 <mariabelen.cepeda@gmail.com>
2026-08-03 21:32:28 -0500

    Initial Selenium framework

---
+++ tests/test_google.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+
+def test_google():
+    driver = webdriver.Chrome()
+
+    driver.get("https://www.google.com")
+
+    assert "Google" in driver.title
+
+    driver.quit()
